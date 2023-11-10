123621932cc8489c85c64ca59de8f4df3672e452
tenk-laptop <takuo6000@gmail.com>
2023-11-10 16:31:14 +0900

    Add script to generate list of speaker pairs for comparison

---
+++ 4th/src/gen_list.py
@@ -0,0 +1,20 @@
+import os
+
+DIR = os.path.dirname(__file__)
+speakers = ["ayu", "ktg", "shina", "tani", "ymgt", "yskw"]
+
+# thx shinboon_
+with open(f"{DIR}/list.txt", "w") as file:
+    for A_idx in range(len(speakers)):
+        same = 1
+        for wav_a in range(4):
+            for wav_b in range(wav_a+1, 4):
+                pair_str = f"{same} ../wav/{speakers[A_idx]}/{wav_a}.wav ../wav/{speakers[A_idx]}/{wav_b}.wav"
+                file.write(pair_str+'\n')
+    for A_idx in range(len(speakers)):
+        for B_idx in range(A_idx + 1, len(speakers)):
+            for wav_a in range(4):
+                for wav_b in range(4):
+                    same = 0
+                    pair_str = f"{same} ../wav/{speakers[A_idx]}/{wav_a}.wav ../wav/{speakers[B_idx]}/{wav_b}.wav"
+                    file.write(pair_str+'\n')

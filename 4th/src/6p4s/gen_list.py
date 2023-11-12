2c60a802db2e9e25cd81414ec7dba80215e57d92
tenk-laptop <takuo6000@gmail.com>
2023-11-13 01:10:04 +0900

    re directory for speak set

--- 4th/src/6p4s/gen_list.py
+++ 4th/src/6p4s/gen_list.py
@@ -9,12 +9,12 @@ with open(f"{DIR}/list.txt", "w") as file:
         same = 1
         for wav_a in range(4):
             for wav_b in range(wav_a+1, 4):
-                pair_str = f"{same} ../wav/{speakers[A_idx]}/{wav_a}.wav ../wav/{speakers[A_idx]}/{wav_b}.wav"
+                pair_str = f"{same} ../../wav/{speakers[A_idx]}/{wav_a}.wav ../../wav/{speakers[A_idx]}/{wav_b}.wav"
                 file.write(pair_str+'\n')
     for A_idx in range(len(speakers)):
         for B_idx in range(A_idx + 1, len(speakers)):
             for wav_a in range(4):
                 for wav_b in range(4):
                     same = 0
-                    pair_str = f"{same} ../wav/{speakers[A_idx]}/{wav_a}.wav ../wav/{speakers[B_idx]}/{wav_b}.wav"
+                    pair_str = f"{same} ../../wav/{speakers[A_idx]}/{wav_a}.wav ../../wav/{speakers[B_idx]}/{wav_b}.wav"
                     file.write(pair_str+'\n')
